e08f0a309db820027fff89ca72c1aed524009901
Mike Chao <aaa3air@gmail.com>
2023-07-12 14:51:00 +0100

    Adding model file for sequelize-cli

---
+++ models/models.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("sqlite:db.sqlite3:");
+const User = sequelize.define("User", {
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+const Lottery = sequelize.define("Lottery", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: DataTypes.STRING,
+  startTime: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  endTime: DataTypes.DATE,
+  bannerURL: DataTypes.STRING,
+  proof: DataTypes.STRING,
+  prizes: DataTypes.STRING,
+  missions: DataTypes.STRING,
+});
+
+(async () => {
+  await sequelize.sync();
+})();
+
+module.exports = { User, Lottery };
